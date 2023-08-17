2d1ead92ab1ce42d2d25c58412b02bd01adf6c50
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-08-16 21:39:06 -0700

    added resolvers and typeDefs into schemas folder

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,60 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id });
+      }
+      throw new Error('Not logged in');
+    }
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!user || !correctPw) {
+        throw new Error('Incorrect credentials');
+      }
+      
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: input } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new Error('You need to be logged in');
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new Error('You need to be logged in');
+    }
+  }
+};
+
+module.exports = resolvers;
